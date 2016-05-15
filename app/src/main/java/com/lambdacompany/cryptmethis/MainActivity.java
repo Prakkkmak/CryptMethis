12fb452b37ac80dd7aa4eaee847900769e6c0a92
lstaff <prakkmak@gmail.com>
2016-05-15 17:18:21 +0200

    Add Ads

--- app/src/main/java/com/lambdacompany/cryptmethis/MainActivity.java
+++ app/src/main/java/com/lambdacompany/cryptmethis/MainActivity.java
@@ -2,6 +2,9 @@ package com.lambdacompany.cryptmethis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
     }
 }
