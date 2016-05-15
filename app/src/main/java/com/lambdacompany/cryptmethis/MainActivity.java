4c657768cf570ab2c032868de01da7b055144530
lstaff <prakkmak@gmail.com>
2016-05-15 23:03:49 +0200

    Début Encryptage

--- app/src/main/java/com/lambdacompany/cryptmethis/MainActivity.java
+++ app/src/main/java/com/lambdacompany/cryptmethis/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private String encrypter_text;
     private int encrypter_numer;
 
+    private TextView encrypted_result;
     private Button but;
 
     @Override
@@ -29,19 +30,30 @@ public class MainActivity extends AppCompatActivity {
         mAdView.loadAd(adRequest);
 
         but = (Button)findViewById(R.id.button);
+        encrypted_result = (TextView)findViewById(R.id.textView3);
 
         but.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                encrypter_numer = Integer.parseInt(((EditText)findViewById(R.id.editText)).getText().toString());
-                encrypter_text = ((EditText)findViewById(R.id.editText2)).getText().toString();
+            public void onClick(View v) {
+                encrypter_numer = Integer.parseInt(((EditText) findViewById(R.id.editText)).getText().toString());
+                encrypter_text = ((EditText) findViewById(R.id.editText2)).getText().toString();
+                encrypted_result.setText(Encrypt(encrypter_numer, encrypter_text));
             }
         });
 
     }
 
-    public String Encrypt(int encrypted_number, String str){
-        //TODO
-        return "Ok";
+    public String Encrypt(int encrypter_number, String str){
+        StringBuilder encrypted_result = new StringBuilder(str);
+        int init_encrypter_number = encrypter_number; // Initialisation du nombre de départ
+        int code; // chiffre éctrait à chaque fois ( Pour 458 on aura 4 , 5 , 8)
+        for(int i=0;i<encrypted_result.length();i++){
+            if (encrypter_number ==0){ // Pour reinitialiser le nombre
+                encrypter_number = init_encrypter_number;
+            }
+            code = encrypter_number%10;// On prend le code
+            encrypter_number/=10;//On on transforme le nombre
+        }
+        return encrypted_result.toString();
     }
 }
